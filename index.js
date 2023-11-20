7cf23582638dfdb76d694e63ae0b7384a25941c2
BobbyK1996 <bobbykamal@hotmail.co.uk>
2023-11-20 23:13:39 +0000

    installed chokidar and added code to watch for add, change and unlink events for files in the current directory

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 #!/usr/bin/env node
 
-console.log("hi");
+import chokidar from "chokidar";
+
+chokidar
+  .watch(".")
+  .on("add", () => console.log("FILE ADDED"))
+  .on("change", () => console.log("FILE CHANGED"))
+  .on("unlink", () => console.log("FILE UNLINKED"));
